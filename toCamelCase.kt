efb6900560a5f9de35f75f4c5ec06f9d6f96d226
Mohammad Osama <92642692+mossssama@users.noreply.github.com>
2022-06-29 05:57:51 +0200

    Convert string to camel case

--- toCamelCase.kt
+++ toCamelCase.kt
@@ -1,7 +1,7 @@
 fun toCamelCase(str:String): String{
-    var str=str.replace(Regex("[-,_]")," ")   // Replace _ & -
+    var str=str.replace(Regex("[-,_]")," ")              // Replace _ & -
 
-    var words=str.split(" ").toMutableList()    // Spliting Words
+    var words=str.split(" ").toMutableList()             // Spliting Words
     str=""                                               // Store new String
     var skip=0
     for (word in words) {                                //concatenating words after capitalizing
